ced95d86c8dd83830939c59e39b462eb9f3e66c3
MatheusHenrique055 <matheus.rsantos@sptech.school>
2023-05-07 11:44:14 -0300

    Corrigindo relacionamentos fracos no BD

--- Banco de Dados/OtakuHub.sql
+++ Banco de Dados/OtakuHub.sql
@@ -23,7 +23,7 @@ CREATE TABLE publicacao (
 );
 
 CREATE TABLE comentario (
-    id_comentario INT PRIMARY KEY AUTO_INCREMENT,
+    id_comentario INT AUTO_INCREMENT,
     texto VARCHAR(250),
     dt_comentario DATETIME DEFAULT CURRENT_TIMESTAMP,
     fk_publicacao INT,
@@ -31,7 +31,8 @@ CREATE TABLE comentario (
     CONSTRAINT fk_comentario_publicacao FOREIGN KEY (fk_publicacao)
         REFERENCES publicacao (id_publicacao),
     CONSTRAINT fk_comentario_usuario FOREIGN KEY (fk_usuario)
-        REFERENCES usuario (id_usuario)
+        REFERENCES usuario (id_usuario),
+    CONSTRAINT pk_comentario PRIMARY KEY (id_comentario , fk_publicacao , fk_usuario)
 );
 
 CREATE TABLE curtida (
@@ -42,7 +43,8 @@ CREATE TABLE curtida (
     CONSTRAINT fk_curtida_publicacao FOREIGN KEY (fk_publicacao)
         REFERENCES publicacao (id_publicacao),
     CONSTRAINT fk_curtida_usuario FOREIGN KEY (fk_usuario)
-        REFERENCES usuario (id_usuario)
+        REFERENCES usuario (id_usuario),
+    CONSTRAINT pk_comentario PRIMARY KEY (id_curtida , fk_publicacao , fk_usuario)
 );
 
 CREATE TABLE estudio_editora (
@@ -52,9 +54,15 @@ CREATE TABLE estudio_editora (
     CONSTRAINT chkEstudioEditora CHECK (tipo IN ('estudio' , 'editora'))
 );
 
+create table autor (
+	id_autor int primary key auto_increment,
+    nome varchar(45),
+    biografia varchar(2000),
+    dt_nascimento date
+);
 
 CREATE TABLE anime_manga (
-    id_anime_manga INT PRIMARY KEY AUTO_INCREMENT,
+    id_anime_manga INT AUTO_INCREMENT,
     tipo CHAR(5),
     CONSTRAINT chk_anime_manga CHECK (tipo IN ('anime' , 'manga')),
     titulo VARCHAR(50),
@@ -63,7 +71,8 @@ CREATE TABLE anime_manga (
     lancamento DATE,
     fk_estudio_editora VARCHAR(45),
     CONSTRAINT fk_estudio_editora FOREIGN KEY (fk_estudio_editora)
-        REFERENCES estudio_editora (id_estudio_editora)
+        REFERENCES estudio_editora (id_estudio_editora),
+    CONSTRAINT pk_anime_manga PRIMARY KEY (id_anime_manga , fk_estudio_editora)
 );
 
 
