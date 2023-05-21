f14e99279e9bc9881357c022e70198a1c494ca23
MatheusHenrique055 <matheus.rsantos@sptech.school>
2023-05-21 17:10:17 -0300

    Work in Progress

--- web_data_viz/site/src/controllers/usuarioController.js
+++ web_data_viz/site/src/controllers/usuarioController.js
@@ -100,9 +100,46 @@ function cadastrar(req, res) {
     }
 }
 
+function votar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var votos = req.body.nomeServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (nick == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (nascimento == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, nick, nascimento, email, senha)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
     entrar,
     cadastrar,
     listar,
-    testar
+    testar,
+    votar
 }
